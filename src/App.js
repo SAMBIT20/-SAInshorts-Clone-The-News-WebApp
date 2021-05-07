697801c2f46d691a4f4e64d87221508488986e3a
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2021-05-07 10:42:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   const newsApi = async () => {
     try {
-      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+   
       const news = await axios.get(
         `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&pageSize=${loadMore}&category=${category}`
       );
